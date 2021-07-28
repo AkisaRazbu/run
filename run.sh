2f0d9900c3dcd1e315f12a5b9d8e9f849e3e706d
Akisa <akisa@auscwitz.com>
2021-07-28 08:26:16 +0200

    Self-documentation

--- run.sh
+++ run.sh
@@ -5,19 +5,25 @@ export FNAME=$2
 
 cd $PWD
 
+# CPP runner
+
 if [ $PLANG = cpp ]
 then
     g++ -Wall -march=native -O2 $FNAME.$PLANG -o $FNAME
     ./$FNAME
 fi
 
-if [ $PLANG = py ]
-then
-    python $FNAME.$PLANG ; printf "\nAborted\n" 
-fi
+# C runner
 
 if [ $PLANG = c ]
 then
 	gcc -Wall -march=native -O2 $FNAME.$PLANG -o $FNAME
 	./$FNAME
+fi
+
+# Python runner
+
+if [ $PLANG = py ]
+then
+    python $FNAME.$PLANG ; printf "\nAborted\n" 
 fi
